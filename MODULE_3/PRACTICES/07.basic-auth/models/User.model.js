e21930c5c10b899e581710876fde8abc1a3f1666
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-03-17 11:04:03 -0400

    save

--- MODULE_3/PRACTICES/07.basic-auth/models/User.model.js
+++ MODULE_3/PRACTICES/07.basic-auth/models/User.model.js
@@ -1,25 +1,31 @@
 const { Schema, model } = require("mongoose");
 
+// const userSchema = new Schema(
+//     {
+//         username: {
+//             type: String,
+//             trim: true,
+//             required: [true, 'Username is required.'],
+//             unique: true
+//         },
+//         email: {
+//             type: String,
+//             required: [true, 'Email is required.'],
+//             unique: true,
+//             lowercase: true,
+//             trim: true
+//         },
+//         passwordHash: {
+//             type: String,
+//             required: [true, 'Password is required.']
+//         }
+//     },
+//     { timestamps: true }
+// );
+
 const userSchema = new Schema(
     {
-        username: {
-            type: String,
-            trim: true,
-            required: [true, 'Username is req2uired.'],
-            unique: true
-        },
-        email: {
-            type: String,
-            required: [true, 'Email is requ2ired.'],
-            unique: true,
-            lowercase: true,
-            trim: true
-        },
-        // add password property here
-        passwordHash: {
-            type: String,
-            required: [true, 'Password is re2quired.']
-        }
+        name: String,
     },
     { timestamps: true }
 );
--- MODULE_3/PRACTICES/07.basic-auth/routes/user.routes.js
+++ MODULE_3/PRACTICES/07.basic-auth/routes/user.routes.js
@@ -11,6 +11,13 @@ router.get("/", async (req, res) => {
     } catch (error) {res.status(500).json({ error: error.message })}
 });
 
+// router.post("/", async (req, res) => {
+//     try {
+//         const newUser = await User.create(req.body);
+//         res.status(200).json(newUser);
+//     } catch (error) {res.status(500).json({ error: error.message })};
+// });
+
 router.post("/", async (req, res) => {
     try {
         const newUser = await User.create(req.body);
